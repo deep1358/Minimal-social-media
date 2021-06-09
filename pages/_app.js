e4eff6e7e7b52bc1cb4d02107727dfdac70cd837
Deep Shah <deepshah1358@gmail.com>
2021-06-09 21:21:04 +0530

    aos removed

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,6 @@
 import Layout from "../components/Layout";
 import { DataProvider } from "../store/GlobalState";
 import "../styles/globals.css";
-import "aos/dist/aos.css";
 import { ToastContainer } from "react-toastify";
 
 function MyApp({ Component, pageProps }) {
